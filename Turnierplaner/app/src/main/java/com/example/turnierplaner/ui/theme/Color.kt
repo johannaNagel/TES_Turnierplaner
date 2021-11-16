bbf76eb3d435e02a5ea00126d3b6425e1b0a788f
Vito Völker <vito.voelker@student.kit.edu>
2021-11-16 16:46:47 +0100

    feat: Changed color pallete

--- Turnierplaner/app/src/main/java/com/example/turnierplaner/ui/theme/Color.kt
+++ Turnierplaner/app/src/main/java/com/example/turnierplaner/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.turnierplaner.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Aquamarine = Color(0xFF75B7D0)
+val NavyBlue = Color(0xFF263F60)
+val Lint = Color(0xFFE4EBE0)
+val Blue = Color(0xFF3982B0)
