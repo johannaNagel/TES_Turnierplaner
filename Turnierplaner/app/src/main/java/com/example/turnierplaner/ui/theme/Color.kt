95b8f2c60d268857319f6fd01bfd411ff6d01225
Vito Völker <vito.voelker@student.kit.edu>
2021-11-16 17:16:58 +0100

    feat: Login and Register Activity

--- Turnierplaner/app/src/main/java/com/example/turnierplaner/ui/theme/Color.kt
+++ Turnierplaner/app/src/main/java/com/example/turnierplaner/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val Aquamarine = Color(0xFF75B7D0)
 val NavyBlue = Color(0xFF263F60)
 val Lint = Color(0xFFE4EBE0)
 val Blue = Color(0xFF3982B0)
+
+val primaryColor = Color(0xFF7048B6)
+val whiteBackground = Color(0xFFF7F7F7)
