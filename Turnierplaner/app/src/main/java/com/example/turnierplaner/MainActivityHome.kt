5ddbc0cff029d915a91ee83032346689faf2b55f
ujuqe <ujuqe@student.kit.edu>
2021-11-24 17:38:28 +0100

    refactor: Reihenfolge der Bottombottons geändert

--- Turnierplaner/app/src/main/java/com/example/turnierplaner/MainActivityHome.kt
+++ Turnierplaner/app/src/main/java/com/example/turnierplaner/MainActivityHome.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.turnierplaner.BottomNavHost
@@ -25,10 +26,10 @@ class MainActivityHome : ComponentActivity() {
         setContent {
             val listItems = listOf(
                 Screens.Home,
+                Screens.Tournament,
                 Screens.Add,
                 Screens.Profile,
-                Screens.Setting,
-                Screens.Tournament
+                Screens.Setting
 
             )
             val navController = rememberNavController()
