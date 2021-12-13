e3ec73634e954c841c0e4dd79db44dbcc5ecef66
Fatih <uelca@student.kit.edu>
2021-12-13 23:58:41 +0100

    fix: #39 PopUp Menu is opening when pressing plus button

--- Turnierplaner/app/src/main/java/com/example/turnierplaner/tournament/TournamentScreen.kt
+++ Turnierplaner/app/src/main/java/com/example/turnierplaner/tournament/TournamentScreen.kt
@@ -3,6 +3,7 @@ package com.example.turnierplaner.tournament
 
 import android.util.Log
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,35 +11,47 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.SportsFootball
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.ExitToApp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -47,6 +60,8 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.Popup
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.turnierplaner.LoginScreens
@@ -120,6 +135,55 @@ Symbol für Tourney Screen
 Ranking Im Turnier
  */
 
+/*
+It is not possible to open PopUpMenu from onclick Method
+Therefore a separate function has to be created
+ */
+
+private val showDialog = mutableStateOf(false)
+@Composable
+fun alert() {
+    var teamname by remember { mutableStateOf("") }
+
+    AlertDialog(
+        modifier = Modifier.padding(24.dp),
+        text = {
+            Column {
+                Text(
+                    modifier = Modifier.padding(vertical = 16.dp),
+                    text = "Add new Team to Tournament"
+                )
+
+                // Rest of the dialog content
+            }
+        },
+        onDismissRequest = {
+            showDialog.value = false
+        },
+        buttons = {
+            OutlinedTextField(
+                modifier = Modifier.padding(vertical = 16.dp),
+                value = teamname,
+                onValueChange = { newTeamname -> teamname = newTeamname },
+                label = { Text(text = "Teamname") },
+            )
+            Button(
+                modifier = Modifier.fillMaxWidth().height(50.dp),
+                enabled = teamname.isNotEmpty(),
+                content = { Text(text = "Add") },
+                onClick = { showDialog.value = false}
+            )
+            Button(
+                modifier = Modifier.fillMaxWidth().height(50.dp),
+                content = { Text(text = "Cancel") },
+                onClick = { showDialog.value = false}
+            )
+        },
+        //modifier = Modifier.size(250.dp, 250.dp),
+        shape = MaterialTheme.shapes.medium,
+    )
+}
+
 @Composable
 fun singleTournamentScreen(navController: NavController, name: String?) {
 
@@ -130,8 +194,11 @@ fun singleTournamentScreen(navController: NavController, name: String?) {
   }
   */
 
-  var tourney = findTournament(name)
+    var tourney = findTournament(name)
 
+    if(showDialog.value) {
+        alert()
+    }
 
     Scaffold(
         topBar = {
@@ -139,16 +206,17 @@ fun singleTournamentScreen(navController: NavController, name: String?) {
                 TopAppBar(
                     backgroundColor = Color.White,
                     elevation = 1.dp,
-                    title = { Text(text = "Tournament") },
+                    title = { Text(text = tourney.name) },
                     actions = {
                         IconButton(
                             onClick = {
+                                showDialog.value = true
                                 //navController.navigate(BottomBarScreens.Add.route)
                             },
                         ) {
                             Icon(
                                 imageVector = Icons.Rounded.Add,
-                                contentDescription = "Button to add new Tournment",
+                                contentDescription = "Button to add new Team",
                             )
 
                         }
